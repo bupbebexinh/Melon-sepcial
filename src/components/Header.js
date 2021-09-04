2d8627ca79861ebaaec1e57539c231f2a1e1547b
Le Anh <huyenanh.lehuyen@gmail.com>
2021-09-04 18:53:39 +0700

    [modify] file

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom'
 import ModalLogin from './ModalLogin';
 import ModalRegist from './ModalRegist';
-import MypageDropdown from './mypage/MypageDropdown';
 
 const Header = ( {isOpen, toggle, keyword, keywordValue, clearSearch} ) => {
   const icon = !isOpen ? <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg> : 
@@ -25,7 +24,6 @@ const Header = ( {isOpen, toggle, keyword, keywordValue, clearSearch} ) => {
         <ul className="flex items-end text-xs font-sans">
           <li className="mr-4">
             My Page
-            {/* <MypageDropdown /> */}
           </li>
           <li>
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
